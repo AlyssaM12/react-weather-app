d4303a511431f80a946371880e65e5c8488c8042
AlyssaM12 <amirra12@gmail.com>
2023-10-23 07:27:13 -0400

    Added GitHub repo link

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App </h1>
+      <footer>
+        This project was coded by Alyssa Mirra and is{" "}
+        <a
+          href="https://github.com/AlyssaM12/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
